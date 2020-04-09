8042b6c5d6010de0d7467a442b4036b3b20f9807
RomanLesovoy <romadesire97@gmail.com>
2020-04-09 09:51:00 +0300

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { NavComponent } from './partials/nav/nav.component';
+import { MainComponent } from './partials/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GalleryComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
