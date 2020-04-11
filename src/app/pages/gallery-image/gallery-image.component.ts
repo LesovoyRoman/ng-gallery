2a1da26f7821c3ce2072a40c4efde5f57f1f91e9
RomanLesovoy <romadesire97@gmail.com>
2020-04-11 19:47:59 +0300

    Render exact image

--- src/app/pages/gallery-image/gallery-image.component.ts
+++ src/app/pages/gallery-image/gallery-image.component.ts
@@ -3,12 +3,12 @@ import { ApiService } from '../../services/api/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ApiImage } from '../../objectTypes/ApiImage';
-import {HelpersService} from '../../services/helpers/helpers.service';
+import { HelpersService } from '../../services/helpers/helpers.service';
 
 @Component({
   selector: 'app-gallery-image',
   templateUrl: './gallery-image.component.html',
-  styleUrls: ['./gallery-image.component.css']
+  styleUrls: ['./gallery-image.component.scss']
 })
 export class GalleryImageComponent implements OnInit {
 
@@ -30,6 +30,7 @@ export class GalleryImageComponent implements OnInit {
       .subscribe((image: ApiImage) => {
         this.isLoading = false;
         this.image = image;
+        this.helpers.loaded(this.spinner);
       }, (err: any) => {
         console.log(err, 'image error');
         this.isLoading = false;
@@ -38,7 +39,6 @@ export class GalleryImageComponent implements OnInit {
   }
 
   ngOnDestroy(): void {
-    console.log('destroy image');
     this.$subscription.unsubscribe();
   }
 }
